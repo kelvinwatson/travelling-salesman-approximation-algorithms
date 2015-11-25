34759c611cdfcce4d299e79b7439ac649b24dd90
watsokel <watsokel@onid.oregonstate.edu>
2015-11-25 13:11:40 -0800

    working on nearest neighbor

--- TSP_algorithms.py
+++ TSP_algorithms.py
@@ -2,18 +2,25 @@ import utility
 
 
 #nearest neighbor (greedy)
-def tsp_nearest_neighbor(distance_matrix, source):
-    #mark all vertices as unvisited
-    #store vertices in adjacency list
-    for i,d in all_distances:
-        single_vertex_data = []
-        if distance_matrix[i][0] == source:
-            if distance_matrix[i][1] != source:
-                single_vertex_data.append(all_distances[i])
-        find_min_destination(source)
+def tsp_nearest_neighbor(adjacency_list, source_vertex):
+    #track visited and unvisited vertices
+    track_visited_vertices = []
+    i = 0
+    while i < len(adjacency_list):
+        track_visited_vertices.append[False]
+    all_visited_flag = False;
 
-#def find_min_destination(u):
+    #assign the source vertex as visited as we are starting there
+    adjacency_list[source_vertex].visited = True
+
+    current_vertex = source_vertex
+    while all_visited_flag is False:
+        find_min_destination(current_vertex, adjacency_list[current_vertex])
+
+
+def find_min_destination(u, adj_u):
     #use binary search to find shortest distance
+    
 
 
 #count number of cities and compute distances between all cities
@@ -36,7 +43,8 @@ all_distances = utility.compute_all_distances(cities)
 print "all_distances: "+str(all_distances) #this example was small enough to print
 
 adjacency_list = utility.create_adjacency_list(all_distances)
-#tsp_nearest_neighbor(adjacency_list,0)
 print "adjacency_list: "
 for vert in adjacency_list:
     print vert.adjacent_vertices
+
+#tsp_nearest_neighbor(adjacency_list,0)
