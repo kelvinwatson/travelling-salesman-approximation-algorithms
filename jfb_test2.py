a6d8a661e823cff715808c89c3a9cb2e3b2899d5
Jeff <muelljef@oregonstate.edu>
2015-11-28 15:35:21 +1000

    modified jfb_test2.py to remove cities as they are not needed

--- jfb_test2.py
+++ jfb_test2.py
@@ -3,13 +3,13 @@ import re, math, csv, os
 
 #saves each vertex/city as a row [vertex num, x coord, y coord] in the cities 2D matrix
 def read_vertices(filename, num_citities=0):
-    cities = []
+    cities = {}
     with open(filename, 'r') as f:
         for line in f:
             city = []
             for num in line.split():
                 city.append(int(num))
-            cities.append(city)
+            cities[city[0]] = [city[1], city[2]]
 
     #print cities
     return cities #returns: [ [v0, x0, y0], ..., [vn, xn, yn] ]
@@ -19,13 +19,12 @@ cities1 = read_vertices('tsp_example_1.txt')
 #returns dictionary of {city: distance, ...} corresponding to distance from given city parameter
 def compute_distance_from_city_to_all_cities(city, cities):
     city_distances = {}
-    num_cities = len(cities)
     dist = 0
-    for i in range(num_cities):
-            delta_x = cities[city][1] - cities[i][1]
-            delta_y = cities[city][2] - cities[i][2]
+    for to_city, coords in cities.iteritems():
+            delta_x = cities[city][0] - coords[0]
+            delta_y = cities[city][1] - coords[1]
             dist = int(round(math.sqrt(math.pow(delta_x,2) + math.pow(delta_y,2))))
-            city_distances[i] = dist
+            city_distances[to_city] = dist
     #print city_distances
     return city_distances
 
@@ -46,15 +45,19 @@ def find_min_adjacent(adjacent_dict, visited):
 
 #neighbor, weight = find_min_adjacent(adjacent_matrix, visited)
 
-def nn_tsp(cities,starting_vertex):
+def nn_tsp(cities, starting_vertex):
     starting_vertex = 0 #set this to the starting vertex
     visited = [starting_vertex]
+    num_cities = len(cities)
     cost = 0
-    counter = 0
+    num_visited = 1
 
-    while len(cities) > len(visited):
+    while num_cities > num_visited:
         #get the last city visited
         last_node = visited[-1]
+        if num_visited > 2:
+            second_last_node = visited[-2]
+
         #compute the distances to all other cities from the last node
         adjacency_last_node = compute_distance_from_city_to_all_cities(last_node, cities)
         #get the nearest neighbor to the last node
@@ -63,10 +66,12 @@ def nn_tsp(cities,starting_vertex):
         if neighbor == -1:
             raise ValueError, "No path found"
         else:
-            counter += 1
-            print counter
             cost += weight
             visited.append(neighbor)
+            if num_visited > 2:
+                del cities[second_last_node]
+            num_visited += 1
+            print num_visited
 
     print visited
     print cost
